9d70300b3eb87c20f7d26c8e5490937c839e5a10
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-08-15 00:22:32 +0100

    updated

--- src/test/java/com/richards/promeescuous/services/UserServiceTest.java
+++ src/test/java/com/richards/promeescuous/services/UserServiceTest.java
@@ -1,47 +1,98 @@
 package com.richards.promeescuous.services;
 
+
+import africa.semicolon.promeescuous.dtos.responses.GetUserResponse;
 import com.richards.promeescuous.dtos.requests.RegisterUserRequest;
 import com.richards.promeescuous.dtos.responses.ApiResponse;
 import com.richards.promeescuous.dtos.responses.RegisterUserResponse;
-import com.richards.promeescuous.services.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Slf4j
 public class UserServiceTest {
-
     @Autowired
     private UserService userService;
-    private final RegisterUserRequest registerUserRequest = new RegisterUserRequest();
 
+    private RegisterUserRequest registerUserRequest;
+    private RegisterUserResponse registerUserResponse;
     @BeforeEach
     public void setUp(){
-        registerUserRequest.setEmail("nicope8821@viperace.com");
+        //user fills registration form
+        registerUserRequest = new RegisterUserRequest();
+        registerUserRequest.setEmail("rofime9859@royalka.com");
         registerUserRequest.setPassword("password");
+        //user submits form by calling register method
     }
 
     @Test
     public void testThatUserCanRegister(){
-        //user fills registration form
-        //user submits form by calling register method
-        RegisterUserResponse registerUserResponse = userService.register(registerUserRequest);
+        registerUserResponse = userService.register(registerUserRequest);
         assertNotNull(registerUserResponse);
         assertNotNull(registerUserResponse.getMessage());
     }
-
     @Test
     public void testActivateUserAccount(){
-        RegisterUserResponse response = userService.register(registerUserRequest);
-        assertNotNull(response);
-
-        ApiResponse<?> activateUserAccountResponse = userService.activateUserAccount("abc1234.erytuol.67t756");
+        registerUserRequest.setEmail("test@email.com");
+        registerUserResponse=userService.register(registerUserRequest);
+        assertNotNull(registerUserResponse);
+        ApiResponse<?> activateUserAccountResponse =
+                userService.activateUserAccount("abc1234.erytuuoi.67t75646");
         assertThat(activateUserAccountResponse).isNotNull();
-
     }
 
+    @Test
+    public void getUserByIdTest(){
+        userService.register(registerUserRequest);
+        GetUserResponse response = userService.getUserById(1L);
+        assertThat(response).isNotNull();
+        assertThat(response.getEmail()).isEqualTo(registerUserRequest.getEmail());
+    }
+
+    @Test
+    public void getAllUsers(){
+        registerTestUsers();
+        List<GetUserResponse> users = userService.getAllUsers(1, 5);
+        log.info("users-->{}", users);
+        assertThat(users).isNotNull();
+        assertThat(users.size()).isEqualTo(5);
+    }
+
+    private void registerTestUsers() {
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setEmail("john@email.com");
+        request.setPassword("password");
+        userService.register(request);
+
+        request.setEmail("jane@email.com");
+        request.setPassword("password");
+        userService.register(request);
+
+
+        request.setEmail("johnny@email.com");
+        request.setPassword("password");
+        userService.register(request);
+
+        request.setEmail("joey@email.com");
+        request.setPassword("password");
+        userService.register(request);
+
+        request.setEmail("zaza@email.com");
+        request.setPassword("password");
+        userService.register(request);
+
+        request.setEmail("nedu@email.com");
+        request.setPassword("password");
+        userService.register(request);
+    }
 }
--- src/test/java/com/richards/promeescuous/utils/AppUtilsTest.java
+++ src/test/java/com/richards/promeescuous/utils/AppUtilsTest.java
@@ -1,27 +1,35 @@
 package com.richards.promeescuous.utils;
 
-import com.richards.promeescuous.utils.AppUtils;
+import com.richards.promeescuous.config.AppConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import static com.richards.promeescuous.utils.AppUtils.generateActivationLink;
+import static com.richards.promeescuous.utils.AppUtils.generateToken;
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 @Slf4j
-public class AppUtilsTest {
+class AppUtilsTest {
+
+    @Autowired
+    private AppConfig appConfig;
 
     @Test
     public void testGenerateActivationLink(){
-        String activationLink = AppUtils.generateActivationLink("test@gmail.com");
-        log.info("activationLink ->{}", activationLink);
+        String activationLink = generateActivationLink(appConfig.getBaseUrl(), "test@email.com");
+        log.info("activation link -->{}", activationLink);
         assertThat(activationLink).isNotNull();
         assertThat(activationLink).contains("http://localhost:8080/activate");
     }
 
+
     @Test
-    public void generateTokenTest(){
-        String email = "test@gmail.com";
-        String token = AppUtils.generateToken(email);
-        log.info("generated token->{}", token);
+    public void generateTokenTest() {
+        String email = "test@email.com";
+        String token = generateToken(email);
+        log.info("generated token--->{}", token);
         assertThat(token).isNotNull();
     }
 }
