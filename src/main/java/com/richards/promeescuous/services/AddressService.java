817d6cb2890f70c97615c04c7801690fd1125452
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-08-24 09:45:34 +0100

    updated

--- src/main/java/com/richards/promeescuous/services/AddressService.java
+++ src/main/java/com/richards/promeescuous/services/AddressService.java
@@ -1,4 +1,22 @@
 package com.richards.promeescuous.services;
+import africa.semicolon.promeescuous.dtos.requests.AddressCreationRequest;
+import africa.semicolon.promeescuous.dtos.requests.AddressUpdateRequest;
+import africa.semicolon.promeescuous.dtos.responses.AddressCreationResponse;
+import africa.semicolon.promeescuous.dtos.responses.AddressUpdateResponse;
+import africa.semicolon.promeescuous.dtos.responses.GetAddressResponse;
+import africa.semicolon.promeescuous.models.Location;
+
+import java.util.List;
+
 
 public interface AddressService {
-}
+
+
+        AddressCreationResponse saveAddress(AddressCreationRequest addressCreationRequest);
+        AddressUpdateResponse updateAddress(AddressUpdateRequest addressUpdateRequest);
+        List<GetAddressResponse> getAllAddresses();
+        GetAddressResponse getAddressById(Long id);
+        List<GetAddressResponse> getAddressBy(Location location);
+        void deleteAll();
+    }
+
