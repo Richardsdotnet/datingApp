026756bbd9cb7c8558a6637012a537806d351da3
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-09-02 01:48:56 +0100

    updated

--- src/main/java/com/richards/promeescuous/models/Address.java
+++ src/main/java/com/richards/promeescuous/models/Address.java
@@ -1,29 +1,39 @@
 package com.richards.promeescuous.models;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
 
 @Entity
+@Getter
+@Setter
+
 public class Address {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String houseNumber;
-    private String street;
-    private String state;
-    private String country;
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append(houseNumber)
-                .append(",")
-                .append(street)
-                .append(",")
-                .append(state)
-                .append(",")
-                .append(country);
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+        private String houseNumber;
+        private String street;
+        private String state;
+        private String country;
 
-        return builder.toString();
-    }
+        @Override
+        public String toString(){
+            StringBuilder builder = new StringBuilder();
+            builder.append(houseNumber)
+                    .append(",")
+                    .append(street)
+                    .append(",")
+                    .append(state)
+                    .append(",")
+                    .append(country);
+            return builder.toString();
+        }
 }
